0601654cad273b25a39c0e8e9b284efac72327c1
darshankparmar <darshanparmar263@gmail.com>
2025-03-05 22:26:34 +0530

    update vite.config.js Base Path for github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "./", // Add this line for GitHub Pages compatibility
+  base: "/darshankparmar/", // Add this line for GitHub Pages compatibility
   plugins: [
     react(),
     mode === 'development' &&
