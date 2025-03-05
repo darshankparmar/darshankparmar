fd6623daa96290f6ff8f0cfc0e66a7b799368843
darshankparmar <darshanparmar263@gmail.com>
2025-03-05 23:31:01 +0530

    remove not found page redirect

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -12,18 +12,6 @@ const NotFound = () => {
       "404 Error: User attempted to access non-existent route:",
       location.pathname
     );
-    const pathSegmentsToKeep = 1;
-    const l = window.location;
-    const newUrl =
-      l.protocol + "//" + l.hostname + (l.port ? ":" + l.port : "") +
-      l.pathname.split("/").slice(0, 1 + pathSegmentsToKeep).join("/") + "/?/" +
-      l.pathname.slice(1).split("/").slice(pathSegmentsToKeep).join("/").replace(/&/g, "~and~") +
-      (l.search ? "&" + l.search.slice(1).replace(/&/g, "~and~") : "") +
-      l.hash;
-
-    if (l.href !== newUrl) {
-      window.location.replace(newUrl);
-    }
   }, [location.pathname]);
 
   return (
